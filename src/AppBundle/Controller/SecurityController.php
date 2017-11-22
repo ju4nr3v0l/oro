bac37059fa326553103b89ab65a59b42a8d72428
Juan Marulanda <revolucioneducativaitm@gmail.com>
2017-11-22 08:19:53 -0500

    upload vendor folder

---
+++ src/AppBundle/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+     /**
+      * @Route("/login", name="login")
+      */
+    public function loginAction(){
+
+        $authenticationUtils = $this->get('security.authentication_utils');
+        // Captura error de logueo si existe
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // Nombre de usuario ingresado
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
+
+    }
+
+}
