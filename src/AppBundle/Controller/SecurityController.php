8dbdbab0c099401906b2b8392193a2effd55fa72
Juan Marulanda <revolucioneducativaitm@gmail.com>
2017-11-22 11:14:39 -0500

    merge alejandro - juan

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -2,25 +2,30 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Forms\Type\FormTypeLogin;
 
-class SecurityController extends Controller{
-
+class SecurityController extends Controller
+{
     /**
-     * @Route("/acceso" , name="acceso")
+     * @Route("/acceso", name="acceso")
      */
+    public function indexAction(Request $request)
+    {
+        $form = $this->createForm(FormTypeLogin::class, null, array(
+            'action' => $this->generateUrl("acceso")
 
-    public function  loginAction(){
-        return $this->render("security/login.html.twig",[
+            )
+        );
 
+        $form->handleRequest($request);
+
+
+        // replace this example code with whatever you need
+        return $this->render('AppBundle:Login:login.html.twig', [
+            'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @Route("/logout")
-     */
-    public function logoutAction(){
-        throw new \RuntimeException('Esta funcion jamas debe ser llamada directamente');
     }
 }
